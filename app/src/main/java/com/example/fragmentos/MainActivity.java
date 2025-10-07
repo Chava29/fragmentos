4663cb6963c7cb2973895259e547cc86f8486839
Chava29 <salvita2013@gmail.com>
2025-10-07 13:48:35 -0600

    falta terminar el verde

---
+++ app/src/main/java/com/example/fragmentos/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.fragmentos;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button brojito, bverdecito;
+    FragmentTransaction transaction;
+    inicio oinicio;
+    rojo orojo;
+    verde overde;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        brojito = findViewById(R.id.brojo);
+        bverdecito = findViewById(R.id.bverde);
+        brojito.setOnClickListener(this);
+        bverdecito.setOnClickListener(this);
+        oinicio = new inicio();
+        orojo = new rojo();
+        overde = new verde();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorfragmentos, oinicio).commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        String cadenita = ((Button)v).getText().toString();
+        transaction = getSupportFragmentManager().beginTransaction();
+
+        if (cadenita.equals("rojo"))
+            transaction.replace(R.id.contenedorfragmentos, orojo).commit();
+    }
+}
